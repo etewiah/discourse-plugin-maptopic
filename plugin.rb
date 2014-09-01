a041ff5642083cbbfbf3d51bc7ca63fb8b8a83cf
Ed Tewiah <etewiah@hotmail.cim>
2014-09-01 20:07:56 +0200

    Add plugin.rb and override composer template

---
+++ plugin.rb
@@ -0,0 +1,6 @@
+# name: Map-Topic
+# about: GigSounder's Map-Topic plugin for Discourse
+# version: 0.1
+# authors: Ed Tewiah
+
+register_asset "javascripts/discourse/templates/composer.js.handlebars"
