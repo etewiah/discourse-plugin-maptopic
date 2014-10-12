da00a4319850767518d23bf2125e06d69fbed74e
Ed Tewiah <etewiah@hotmail.cim>
2014-10-12 19:46:12 +0100

    Route / to list when view is mobile

--- assets/javascripts/discourse_extensions/discovery_route.js
+++ assets/javascripts/discourse_extensions/discovery_route.js
@@ -2,9 +2,14 @@ Discourse.DiscoveryRoute.reopen({
   beforeModel: function(transition) {
     // to ensure that when discovery route is handling '/'
     //  it redirects to map.root
-    if(location.pathname === "/"){
-    // if (transition.targetName.indexOf('discovery.latest') !== -1) {
-      this.transitionTo('map.root');
+    if (location.pathname === "/") {
+      // if (transition.targetName.indexOf('discovery.latest') !== -1) {
+      if (Discourse.Mobile.mobileView) {
+        this.transitionTo('discovery.latest');
+      } else {
+        this.transitionTo('map.root');
+
+      };
     }
   }
 });
