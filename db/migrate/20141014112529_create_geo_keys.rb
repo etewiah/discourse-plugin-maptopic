fc0269ed7280097b62dd14e7a537556707acd2ef
Ed Tewiah <etewiah@hotmail.cim>
2014-10-14 12:42:08 +0100

    Add geo keys migration

---
+++ db/migrate/20141014112529_create_geo_keys.rb
@@ -0,0 +1,15 @@
+class CreateGeoKeys < ActiveRecord::Migration
+  def change
+    create_table :geo_keys do |t|
+      t.string :display_name
+      t.float :longitude
+      t.float :latitude
+      t.string :bounds_type
+      t.string :bounds_value
+      t.integer :bounds_range
+      t.string :country_lower
+      t.string :city_lower
+      t.string :show_criteria
+    end
+  end
+end
